3aab475f582092acf0ab1a9b295c20c459c7a6d9
Nero Blackstone <gf7600gs@gmail.com>
2024-09-20 00:46:16 +0800

    finish day 3

---
+++ day3-hmall/hm-service/src/main/java/com/hmall/controller/CartController.java
@@ -0,0 +1,54 @@
+package com.hmall.controller;
+
+
+import com.hmall.domain.dto.CartFormDTO;
+import com.hmall.domain.po.Cart;
+import com.hmall.domain.vo.CartVO;
+import com.hmall.service.ICartService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@Tag(name = "购物车相关接口")
+@RestController
+@RequestMapping("/carts")
+@RequiredArgsConstructor
+public class CartController {
+    private final ICartService cartService;
+
+    @Operation(description = "添加商品到购物车")
+    @PostMapping
+    public void addItem2Cart(@Valid @RequestBody CartFormDTO cartFormDTO){
+        cartService.addItem2Cart(cartFormDTO);
+    }
+
+    @Operation(description = "更新购物车数据")
+    @PutMapping
+    public void updateCart(@RequestBody Cart cart){
+        cartService.updateById(cart);
+    }
+
+    @Operation(description = "删除购物车中商品")
+    @DeleteMapping("{id}")
+    public void deleteCartItem(@Param ("购物车条目id")@PathVariable("id") Long id){
+        cartService.removeById(id);
+    }
+
+    @Operation(description = "查询购物车列表")
+    @GetMapping
+    public List<CartVO> queryMyCarts(){
+        return cartService.queryMyCarts();
+    }
+    @Operation(description = "批量删除购物车中商品")
+    @Parameter(name = "ids", description = "购物车条目id集合")
+    @DeleteMapping
+    public void deleteCartItemByIds(@RequestParam("ids") List<Long> ids){
+        cartService.removeByItemIds(ids);
+    }
+}
---
+++ day3-hmall/hm-service/src/main/java/com/hmall/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.hmall.controller;
+
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.domain.dto.OrderFormDTO;
+import com.hmall.domain.vo.OrderVO;
+import com.hmall.service.IOrderService;
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "订单管理接口")
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final IOrderService orderService;
+
+    @Operation(description = "根据id查询订单")
+    @GetMapping("{id}")
+    public OrderVO queryOrderById(@Param ("订单id")@PathVariable("id") Long orderId) {
+        return BeanUtils.copyBean(orderService.getById(orderId), OrderVO.class);
+    }
+
+    @Operation(description = "创建订单")
+    @PostMapping
+    public Long createOrder(@RequestBody OrderFormDTO orderFormDTO){
+        return orderService.createOrder(orderFormDTO);
+    }
+
+    @Operation(description = "标记订单已支付")
+    @Parameter(name = "orderId", description =  "订单id", in= ParameterIn.PATH)
+    @PutMapping("/{orderId}")
+    public void markOrderPaySuccess(@PathVariable("orderId") Long orderId) {
+        orderService.markOrderPaySuccess(orderId);
+    }
+}
